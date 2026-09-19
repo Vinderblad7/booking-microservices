26a6543ac1740f9bd8e74e65cdb8dd43b8a49ac3
Vadim <vadimguleak777@gmail.com>
2026-09-19 17:39:09 +0300

    frontend: add header markup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,14 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
+import Header from './components/header/header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <main>
-      <h1>Travel Booking</h1>
-      <p>Find your perfect trip with our easy-to-use booking platform.</p>
-    </main>
+    <>
+      <Header />
+    </>
   )
 }
 
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import logoBadge from '../../assets/icons/logo-badge.svg';
+
+function Header() {
+    return (
+        <header className="header">
+
+            <div className="logo">
+                <img src={logoBadge} alt="Logo" />
+                <span className="logo-text">Vinderblad & Kudoxq</span>
+            </div>
+            
+            <nav className="navigation">
+                <a href="/directions">Направления</a>
+                <a href="/hotels">Отели</a>
+                <a href="/flights">Авиабилеты</a>
+                <a href="/tours">Готовые туры</a>
+                <a href="/contacts">Контакты</a>
+            </nav>
+
+            <div className="actions">
+                <button className="lang-currency-switcher">RU \ RUB</button>
+                <button className="login-button">Войти</button>
+                <button className="signup-button">Регистрация</button>
+            </div>
+
+        </header>
+    )
+}
+
+export default Header
