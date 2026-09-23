09d3bd81249e344b750e77723dcb1caeb9a6f675
Vadim <vadimguleak777@gmail.com>
2026-09-23 16:26:16 +0300

    frontend: small improve of the searchPanel

--- frontend/src/components/Hero/SearchPanel/SearchPanel.jsx
+++ frontend/src/components/Hero/SearchPanel/SearchPanel.jsx
@@ -1,9 +1,28 @@
-import './SearchPanel.css'
-import fieldIcon1 from '../../../assets/icons/field-icon-bg.svg'
-import fieldIcon2 from '../../../assets/icons/field-icon-bg2.svg'
-import fieldIcon3 from '../../../assets/icons/field-icon-bg3.svg'
+import {useState} from 'react';
+import './SearchPanel.css';
+import fieldIcon1 from '../../../assets/icons/field-icon-bg.svg';
+import fieldIcon2 from '../../../assets/icons/field-icon-bg2.svg';
+import fieldIcon3 from '../../../assets/icons/field-icon-bg3.svg';
 
 function SearchPanel() {
+
+    const [destination, setDestination] = useState('Paris');
+    const [date, setDate] = useState('');
+    const [guests, setGuests] = useState('1 гость');
+
+    function handleSearch() {
+        if (!destination || !date) {
+            console.log('Выберите место и дату')
+            return;
+        }
+
+        console.log({
+            destination,
+            date,
+            guests
+        });
+    };
+
     return (
         <div className="search-panel">
 
@@ -11,8 +30,17 @@ function SearchPanel() {
                 <img src={fieldIcon1} alt="location" className="search-field-icon" />
 
                 <div className="search-field-content">
-                    <span className="search-field-label">Куда летим</span>
-                    <span className="search-field-value">Бали, Индонезия</span>
+                    <span className='search-field-label'>Куда едем</span>
+                    <select className='search-field-select'
+                        value={destination}
+                        onChange={(event) => setDestination(event.target.value)}
+                    >
+                        <option value="Paris">Париж</option>
+                        <option value="Praha">Прага</option>
+                        <option value="Barcelona">Барселона</option>
+                        <option value="Rome">Рим</option>
+                        <option value="Berlin">Берлин</option>
+                    </select>
                 </div>
                 
             </div>
@@ -22,7 +50,13 @@ function SearchPanel() {
 
                 <div className="search-field-content">
                     <span className="search-field-label">Даты</span>
-                    <span className="search-field-value">24 Мая - 8 Июня</span>
+                    
+                    <input 
+                        className="search-field-select-date" 
+                        type='date'
+                        value={date}
+                        onChange={(event) => setDate(event.target.value)}
+                     />
                 </div>
                 
             </div>
@@ -32,12 +66,29 @@ function SearchPanel() {
 
                 <div className="search-field-content">
                     <span className="search-field-label">Кто едет</span>
-                    <span className="search-field-value">2 Взрослых, Бизнес</span>
+                    
+                    <select 
+                        className="search-field-select"
+                        value={guests}
+                        onChange={(event) => setGuests(event.target.value)}
+                    >
+                        <option>1 гость</option>
+                        <option>2 гостя</option>
+                        <option>3 гостя</option>
+                        <option>4 гостя</option>
+                        <option>5 гостей</option>
+                        <option>6 гостей</option>
+                    </select>
                 </div>
                 
             </div>
 
-            <button className="search-field-button">Найти туры</button>
+            <button 
+                className="search-field-button"
+                onClick={handleSearch}
+            >
+                Найти туры
+            </button>
 
         </div>
     );
